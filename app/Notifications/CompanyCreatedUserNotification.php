596e0d47806482b9f0922dc7d050646355cd3098
VincentNdegwa <ndegwavincent7@gmail.com>
2025-07-24 21:39:58 +0300

    refactor: Add Slack notification support for company and welcome user notifications

--- app/Notifications/CompanyCreatedUserNotification.php
+++ app/Notifications/CompanyCreatedUserNotification.php
@@ -5,7 +5,9 @@ namespace App\Notifications;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
-
+use Illuminate\Notifications\Slack\BlockKit\Blocks\ContextBlock;
+use Illuminate\Notifications\Slack\BlockKit\Blocks\SectionBlock;
+use Illuminate\Notifications\Slack\SlackMessage;
 class CompanyCreatedUserNotification extends Notification
 {
     use Queueable;
@@ -23,7 +25,7 @@ class CompanyCreatedUserNotification extends Notification
 
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail', 'slack'];
     }
 
     public function toMail($notifiable)
@@ -39,5 +41,26 @@ class CompanyCreatedUserNotification extends Notification
             ->line('Please do not share your login details with anyone.')
             ->action('Login', url('/login'))
             ->line('Thank you!');
+    }
+    public function toSlack(object $notifiable)
+    {
+        return (new SlackMessage)
+            ->to('#validtrack-companies')
+            ->text('A new company has been registered!')
+            ->headerBlock('Company Registration')
+            ->contextBlock(function (ContextBlock $block) use ($notifiable) {
+                $block->text('A new user has been registered by the system.');
+            })
+            ->sectionBlock(function (SectionBlock $block) use ($notifiable) {
+                $block->text('Company and owner details:');
+                $block->field("*Company Name:*\n" . $this->company->name)->markdown();
+                $block->field("*Company Email:*\n" . $this->company->email)->markdown();
+                $block->field("*Owner Name:*\n" . $notifiable->name)->markdown();
+                $block->field("*Owner Email:*\n" . $notifiable->email)->markdown();
+            })
+            ->dividerBlock()
+            ->sectionBlock(function (SectionBlock $block) {
+                $block->text('Welcome to the platform!');
+            });
     }
 }
--- app/Notifications/WelcomeUserNotification.php
+++ app/Notifications/WelcomeUserNotification.php
@@ -5,6 +5,9 @@ namespace App\Notifications;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Slack\BlockKit\Blocks\ContextBlock;
+use Illuminate\Notifications\Slack\BlockKit\Blocks\SectionBlock;
+use Illuminate\Notifications\Slack\SlackMessage;
 
 class WelcomeUserNotification extends Notification
 {
@@ -19,7 +22,7 @@ class WelcomeUserNotification extends Notification
 
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail', 'slack'];
     }
 
     public function toMail($notifiable)
@@ -31,5 +34,27 @@ class WelcomeUserNotification extends Notification
             ->line('You can now log in and start using your workspace.')
             ->action('Login', url('/login'))
             ->line('Thank you for joining us!');
+    }
+
+    public function toSlack(object $notifiable)
+    {
+        return (new SlackMessage)
+            ->to('#validtrack-companies')
+            ->text('A new company has been registered!')
+            ->headerBlock('Company Registration')
+            ->contextBlock(function (ContextBlock $block) use ($notifiable) {
+                $block->text('A new user has been registered by the system.');
+            })
+            ->sectionBlock(function (SectionBlock $block) use ($notifiable) {
+                $block->text('Company and owner details:');
+                $block->field("*Company Name:*\n" . $this->company->name)->markdown();
+                $block->field("*Company Email:*\n" . $this->company->email)->markdown();
+                $block->field("*Owner Name:*\n" . $notifiable->name)->markdown();
+                $block->field("*Owner Email:*\n" . $notifiable->email)->markdown();
+            })
+            ->dividerBlock()
+            ->sectionBlock(function (SectionBlock $block) {
+                $block->text('Welcome to the platform!');
+            });
     }
 }
